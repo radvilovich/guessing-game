aabd61b1dbf115e447f1c060a76f4a5ea52b6cee
radvilovich <aska_157@mail.ru>
2019-10-14 10:00:23 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
+    min = 0;
+    max = 0;
+    average = 0;
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.round((this.min + this.max) / 2);
+        return this.average;
     }
 
     lower() {
-
+        return this.max = this.average;
     }
 
     greater() {
-
+        return this.min = this.average;
     }
 }
 
